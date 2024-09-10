9e45d31bbcd001521ab57be7f15af96d9d42e843
Benjamin Peters <b.peters@accenture.com>
2024-09-10 17:37:18 +0200

    continuing walkthrough - adding controllers, views and model

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,19 +1,9 @@
 // Use sap.ui.define for controllers and all other JavaScript modules to define a global namespace. With the namespace, the object can be addressed throughout the application.
 sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-], (Controller, MessageToast) => {
+	"sap/ui/core/mvc/Controller"
+ ], (Controller) => {
 	"use strict";
-
+ 
 	return Controller.extend("ui5.walkthrough.controller.App", {
-		onShowHello() {
-			// read msg from i18n model
-			const oBundle = this.getView().getModel("i18n").getResourceBundle();
-			const sRecipient = this.getView().getModel().getProperty("/recipient/name");
-			const sMsg = oBundle.getText("helloMsg", [sRecipient]);
-
-			// show message
-			MessageToast.show(sMsg);
-		}
 	});
-});
+ });
