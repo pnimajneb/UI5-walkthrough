0a68591678382a6c6b06feb4662822bb483fe71e
Benjamin Peters <bpeters@posteo.de>
2024-09-11 09:11:23 +0200

    implementing routing back and history

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,9 +1,11 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], (Controller) => {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/routing/History"
+], (Controller, History) => {
 	"use strict";
 
 	return Controller.extend("ui5.walkthrough.controller.Detail", {
+
 		onInit() {
 			const oRouter = this.getOwnerComponent().getRouter();
 			oRouter.getRoute("detail").attachPatternMatched(this.onObjectMatched, this);
@@ -14,6 +16,18 @@ sap.ui.define([
 				path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
 				model: "invoice"
 			});
+		},
+
+		onNavBack() {
+			const oHistory = History.getInstance();
+			const sPreviousHash = oHistory.getPreviousHash();
+
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				const oRouter = this.getOwnerComponent().getRouter();
+				oRouter.navTo("overview", {}, true);
+			}
 		}
 	});
 });
