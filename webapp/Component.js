4d8f6ebd343c15a10d11c073d518d10a81fb0b71
Benjamin Peters <bpeters@posteo.de>
2024-09-11 07:29:09 +0200

    implementing routing with parameters

--- webapp/Component.js
+++ webapp/Component.js
@@ -6,7 +6,6 @@ sap.ui.define([
 
 	return UIComponent.extend("ui5.walkthrough.Component", {
 		metadata: {
-			//test
 			// implicitly sets both the component's rootView and its router configuration to "async": true
 			interfaces: ["sap.ui.core.IAsyncContentCreation"],
 			manifest: "json"
